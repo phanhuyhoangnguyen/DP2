92da368849fbeb2030488d9ab147751524cda903
phanhuyhoangnguyen <101042618@student.swin.edu.au>
2017-10-05 20:36:33 +1100

    fix_bugs

--- javascript/display_popular_item.js
+++ javascript/display_popular_item.js
@@ -31,33 +31,29 @@ function display_popular_item()
         var year;
         var vars;
 
-        if (option == "day_view" || option == "month_view") {
-
-                        if (option == "day_view") {
-
-                            date = time_splitter[0];
-                            month = time_splitter[1];
-                            year = time_splitter[2];
-                            vars = "$option="+encodeURIComponent(option)+"&select_date="+encodeURIComponent(date)+"&select_month="+encodeURIComponent(month)+"&select_year="+encodeURIComponent(year);
-                        }
-
-                        else if (option == "month_view") {
-
-                            month = time_splitter[0];
-                            year = time_splitter[1];
-                            vars = "$option="+encodeURIComponent(option)+"&select_month=" + encodeURIComponent(month) + "&select_year=" + encodeURIComponent(year);
-                        }
-                        else {
-                            year = time_splitter[0];
-                            vars = "$option="+encodeURIComponent(option)+ "&select_year=" + encodeURIComponent(year);
-                        }
-
-                        //alert(date + "/" + month + "/" + year);
-            //alert(option);
-            alert(vars);
-
+        if (option == "day_view") {
+            date = time_splitter[0];
+            month = time_splitter[1];
+            year = time_splitter[2];
+            vars = "&option="+encodeURIComponent(option)+"&select_date="+encodeURIComponent(date)+"&select_month="+encodeURIComponent(month)+"&select_year="+encodeURIComponent(year);
         }
 
+        else if (option == "month_view") {
+                            month = time_splitter[0];
+                            year = time_splitter[1];
+                            vars = "&option="+encodeURIComponent(option)+"&select_month=" + encodeURIComponent(month) + "&select_year=" + encodeURIComponent(year);
+        }
+
+
+        else if (option == "year_view") {
+            year = time_splitter[0];
+            vars = "&option="+encodeURIComponent(option)+ "&select_year=" + encodeURIComponent(year);
+        }
+
+        else {
+            vars = "&option="+encodeURIComponent("")+"&select_date="+encodeURIComponent("")+"&select_month="+encodeURIComponent("")+"&select_year="+encodeURIComponent("");
+
+        }
         xHRObject.open("POST", url, true);
         xHRObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xHRObject.send(vars);
