ae6eab0395859a937e04b71b41b8b27fc23cefd1
phanhuyhoangnguyen <101042618@student.swin.edu.au>
2017-10-08 00:43:41 +1100

    add ajax refund report

---
+++ javascript/display_refund_report.js
@@ -0,0 +1,39 @@
+/**
+ * Created by vietnguyenswin on 9/17/17.
+ */
+var xHRObject = false;
+if (window.XMLHttpRequest)
+{
+    xHRObject = new XMLHttpRequest();
+}
+else if (window.ActiveXObject)
+{
+    xHRObject = new ActiveXObject("Microsoft.XMLHTTP");
+}
+function display_refund_report()
+{
+    if(xHRObject)
+    {
+        var url = "php/display_refund_report.php";
+
+        var month = document.getElementById("select_month").value;
+        var year = document.getElementById("select_year").value;
+
+        if (month == "" || year == "")
+            alert("Please select month and year");
+
+        var vars = "select_month="+encodeURIComponent(month)+"&select_year="+encodeURIComponent(year);
+        xHRObject.open("POST", url, true);
+        xHRObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xHRObject.send(vars);
+
+        xHRObject.onreadystatechange = function ()
+        {
+            if ((xHRObject.readyState == 4) && (xHRObject.status == 200))
+            {
+                return document.getElementById("echo_result").innerHTML = this.responseText;
+            }
+        }
+    }
+    return false;
+}
